26c43a26b48f8dfb7482d4fd48b2cb847982a675
guthembergS <gutheaugusto123@gmail.com>
2020-08-14 01:11:48 -0300

    start user registration api

---
+++ src/main/java/com/userregistrationapi/UserregistrationapiApplication.java
@@ -0,0 +1,13 @@
+package com.userregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistrationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserregistrationapiApplication.class, args);
+	}
+
+}
